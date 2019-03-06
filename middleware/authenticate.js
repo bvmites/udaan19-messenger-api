fd040e673a5152d3de42aa96fc3709f2ff8c6307
rushi7997 <ramirushi7997@gmail.com>
2019-03-07 01:22:57 +0530

    lastly added route for getting multiple participants

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (request, response, next) => {
 	try {
-
+		console.log("###############################################################################");
 		console.log(request.body);
 		const token = request.header('Authorization');
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
